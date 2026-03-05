4c205c8662e7a734cb43114cbb9aa894bb8ad762
Danny <kilatomerat@gmail.com>
2026-03-05 23:34:29 +0200

    Adjustment

--- web/src/main/java/bg/tu_varna/sit/recruitment_agency/matching/web/MatchingApplication.java
+++ web/src/main/java/bg/tu_varna/sit/recruitment_agency/matching/web/MatchingApplication.java
@@ -2,9 +2,15 @@ package bg.tu_varna.sit.recruitment_agency.matching.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "bg.tu_varna.sit.recruitment_agency.matching.persistence")
+@EntityScan(basePackages = "bg.tu_varna.sit.recruitment_agency.matching.persistence")
+@EnableFeignClients(basePackages = "bg.tu_varna.sit.recruitment_agency.matching.core")
 @EnableDiscoveryClient
 public class MatchingApplication {
 
