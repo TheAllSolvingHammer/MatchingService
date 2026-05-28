5e2a9c0d8b95126ccd71875e6a310d453fc53dde
Danny <kilatomerat@gmail.com>
2026-05-28 21:44:38 +0300

    Added small Rabbit adjustment

--- web/src/main/java/bg/tu_varna/sit/recruitment_agency/matching/web/config/RabbitMQConfig.java
+++ web/src/main/java/bg/tu_varna/sit/recruitment_agency/matching/web/config/RabbitMQConfig.java
@@ -5,7 +5,9 @@ import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
     // Matches the queue name defined in your application.properties
     @Value("${rabbitmq.queue.application-submitted:application-submitted-queue}")
